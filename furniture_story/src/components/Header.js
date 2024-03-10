6ec9c489276dd45bbb1327844f3452554a9a857b
Rom-Freedom <romeo.dark@mail.ru>
2024-03-10 12:03:23 +0300

    nav menu

--- furniture_story/src/components/Header.js
+++ furniture_story/src/components/Header.js
@@ -5,6 +5,11 @@ export default function Header() {
     <header>
         <div>
             <span className="logo">House Staff</span>
+            <ul className="nav">
+                <li>About</li>
+                <li>Contacts</li>
+                <li>Room</li>
+            </ul>
         </div>
         <div className="presentation"></div>
     </header>
