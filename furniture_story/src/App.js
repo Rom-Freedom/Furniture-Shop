b918d66b1a8044ff81c3a55f689afceb8bd70ae8
Rom-Freedom <romeo.dark@mail.ru>
2024-03-11 00:23:20 +0300

    display goods

--- furniture_story/src/App.js
+++ furniture_story/src/App.js
@@ -1,13 +1,73 @@
+import React from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Items from './components/Items'
 
-function App() {
-  return (
-    <div className='wrapper'>
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Armchair',
+          img: 'armchair-3.jpg',
+          desc: 'A comfortable gray armchair ',
+          category: 'chairs',
+          price: '47.79'
+        },
+        {
+          id: 2,
+          title: 'Table',
+          img: 'table.jpg',
+          desc: 'A style gray table',
+          category: 'tables',
+          price: '130.99'
+        },
+        {
+          id: 3,
+          title: 'Sofa',
+          img: 'sofa.jpg',
+          desc: 'A uncredible convenient sofa',
+          category: 'Sofa',
+          price: '175.23'
+        },
+        {
+          id: 4,
+          title: 'Lamp',
+          img: 'light.jpg',
+          desc: 'A style lamp',
+          category: 'Lights',
+          price: '30.99'
+        },
+        {
+          id: 5,
+          title: 'Shelf',
+          img: 'shelf.jpg',
+          desc: 'A pleasure shelf',
+          category: 'Shelfs',
+          price: '120.50'
+        },
+        {
+          id: 6,
+          title: 'Kitchen',
+          img: 'kitchen.jpg',
+          desc: 'Furniture for your kitchen',
+          category: 'Kitchens',
+          price: '1390.00'
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className='wrapper'>
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
