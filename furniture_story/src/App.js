50e569a5d885089e0ee218de9554be262644d0f4
Rom-Freedom <romeo.dark@mail.ru>
2024-03-10 11:48:14 +0300

    make up, style header and footer section

--- furniture_story/src/App.js
+++ furniture_story/src/App.js
@@ -1,7 +1,12 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div>Furniture Shop</div>
+    <div className='wrapper'>
+      <Header />
+      <Footer />
+    </div>
   );
 }
 
